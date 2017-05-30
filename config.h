36310c2cc5bfe8507cf630e5f78226c4646469e3
Bradley Bell <bradley.bell@gmail.com>
2017-05-30 00:55:50 -0700

    adapted for 20x4 vfd. formatting changes

--- config.h
+++ config.h
@@ -32,42 +32,45 @@
 #define default_backlash_moveB 92 // Schritte zur kompensation
 // ---------------------------------------------------------------------------
 
-#define encoder_PinA 17
-#define encoder_PinB 18
-#define encoderEXT_PinA 19
-#define encoderEXT_PinB 20
+#define encoder_PinA 7 //17
+#define encoder_PinB 8 //18
+//#define encoderEXT_PinA 19
+//#define encoderEXT_PinB 20
 
 #define spindle_PinA 2
 #define spindle_PinB 3
-#define spindle_PinZ 21 
+//#define spindle_PinZ 21
 
+//#define S1  42
+//#define S2  43
+//#define S3  44
+//#define S4  45
+//#define S5  46
+//#define S6  47
+#define up  A1 // 51
+#define down 12 // 49
+#define right 11 // 50
+#define left A0 // 48
+#define encoder 10 // 40
 
-#define S1  42
-#define S2  43
-#define S3  44
-#define S4  45
-#define S5  46
-#define S6  47
-#define up  51
-#define down  49
-#define right  50
-#define left  48
-#define encoder  40
+#define led2  13//53 // load/save data
+#define led1  13//52 // mode 0/1
+#define led3  13//30 // spindleZ activity
+#define led4  13//31 // stepper running
+//#define out3  8 //unused
 
-#define led2  53
-#define led1  52
-#define led3  30
-#define led4  31
-#define out3  8
+#define vfd_sio 4
+#define vfd_stb 5
+#define vfd_sck 6
 
-#define dirpin 10 //pin für Richtung Z
-#define steppin  9 //pin für Schritt Z 
-
-#define dirpinB 13 //pin für Richtung X
-#define steppinB  12 //pin für Schritt X
-
-#define tweeter  32
-#define relais A7 
+//#define dirpin    10 //pin für Richtung Z
+//#define steppin   9 //pin für Schritt Z
+//
+//#define dirpinB   13 //pin für Richtung X
+//#define steppinB  12 //pin für Schritt X
+//
+//#define tweeter  32
+//#define relais A7
 
 #define stepper_delay 10
 
@@ -89,86 +92,86 @@
 
 
 typedef struct {
-	//! What part of the speed ramp we are in.
-	unsigned char run_state : 3;
-	//! Direction stepper motor should move.
-	unsigned char dir : 1;
-	
-	unsigned char dir_old : 1;
-	//! Peroid of next timer delay. At start this value set the accelration rate.
-	unsigned int step_delay;
-	//! What step_pos to start decelaration
-	unsigned int decel_start;
-	//! Sets deceleration rate.
-	signed int decel_val;
-	//! Minimum time delay (max speed)
-	signed int min_delay;
-	//! Counter used when accelerateing/decelerateing to calculate step_delay.
-	signed int accel_count;
+  //! What part of the speed ramp we are in.
+  unsigned char run_state : 3;
+  //! Direction stepper motor should move.
+  unsigned char dir : 1;
+
+  unsigned char dir_old : 1;
+  //! Peroid of next timer delay. At start this value set the accelration rate.
+  unsigned int step_delay;
+  //! What step_pos to start decelaration
+  unsigned int decel_start;
+  //! Sets deceleration rate.
+  signed int decel_val;
+  //! Minimum time delay (max speed)
+  signed int min_delay;
+  //! Counter used when accelerateing/decelerateing to calculate step_delay.
+  signed int accel_count;
 } speedRampData;
 
 speedRampData srd;
 
 typedef struct {
-	//! What part of the speed ramp we are in.
-	unsigned char run_state : 3;
-	//! Direction stepper motor should move.
-	unsigned char dir : 1;
-	
-	unsigned char dir_old : 1;
-	//! Peroid of next timer delay. At start this value set the accelration rate.
-	unsigned int step_delay;
-	//! What step_pos to start decelaration
-	unsigned int decel_start;
-	//! Sets deceleration rate.
-	signed int decel_val;
-	//! Minimum time delay (max speed)
-	signed int min_delay;
-	//! Counter used when accelerateing/decelerateing to calculate step_delay.
-	signed int accel_count;
+  //! What part of the speed ramp we are in.
+  unsigned char run_state : 3;
+  //! Direction stepper motor should move.
+  unsigned char dir : 1;
+
+  unsigned char dir_old : 1;
+  //! Peroid of next timer delay. At start this value set the accelration rate.
+  unsigned int step_delay;
+  //! What step_pos to start decelaration
+  unsigned int decel_start;
+  //! Sets deceleration rate.
+  signed int decel_val;
+  //! Minimum time delay (max speed)
+  signed int min_delay;
+  //! Counter used when accelerateing/decelerateing to calculate step_delay.
+  signed int accel_count;
 } speedRampData_temp;
 
 speedRampData_temp srd_temp;
 
 
 struct GLOBAL_FLAGS {
-	//! True when stepper motor is running.
-	unsigned char running:1;
-	//! True when uart has received a string (ended with '/r').
-	unsigned char cmd:1;
-	//! Dummy bits to fill up a byte.
-	
-	unsigned char thread:1;	
-	
-	unsigned char encoder_trigger:1;
-	
-	unsigned char backlash_trigger:1;
-	
-	unsigned char backlash:1;
-	
-	unsigned char key_pressed:1;
-	
-	unsigned char goback_trigger:1;
-	
-	unsigned char dir:1;
-	
+  //! True when stepper motor is running.
+  unsigned char running: 1;
+  //! True when uart has received a string (ended with '/r').
+  unsigned char cmd: 1;
+  //! Dummy bits to fill up a byte.
+
+  unsigned char thread: 1;
+
+  unsigned char encoder_trigger: 1;
+
+  unsigned char backlash_trigger: 1;
+
+  unsigned char backlash: 1;
+
+  unsigned char key_pressed: 1;
+
+  unsigned char goback_trigger: 1;
+
+  unsigned char dir: 1;
+
 };
 
-struct GLOBAL_FLAGS status = {FALSE, FALSE, FALSE, FALSE,FALSE,FALSE};
-	
-	struct config_t{
-		long thread_pitch ; 
-		long thread_length ;
-		long grind_way;
-		long grind_speed ;
-		long cutting_way ;
-		long cutting_speed;
-		long move_way ;
-		long move_fast_speed ;
-		long move_slow_speed ;
-		long fast_move ;
-		long slow_move ;
-		long delay_move ;
-		long backlash_move;
-	}configuration;
+struct GLOBAL_FLAGS status = {FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE};
+
+struct config_t {
+  long thread_pitch ;
+  long thread_length ;
+  long grind_way;
+  long grind_speed ;
+  long cutting_way ;
+  long cutting_speed;
+  long move_way ;
+  long move_fast_speed ;
+  long move_slow_speed ;
+  long fast_move ;
+  long slow_move ;
+  long delay_move ;
+  long backlash_move;
+} configuration;
 
